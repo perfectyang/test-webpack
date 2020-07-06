757a90e27833f5fb2bd9d1fd46a0e1ba3cd1f9c6
perfectyang <2406790623@qq.com>
2020-07-06 14:15:19 +0800

    fix

--- src/view/app.vue
+++ src/view/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     <div>在这里测试啊测试自动打包---没test</div>
+    <button @click="e.g=2;">click</button>
   </div>
 </template>
 
@@ -8,6 +9,18 @@
 export default {
   data () {
     return {
+      e: {
+        g: 1
+      }
+    }
+  },
+  watch: {
+    e: {
+      immediate: true,
+      deep: true,
+      handler(newValue, oldValue) {
+        console.log(newValue, oldValue) 
+      }
     }
   },
   methods: {
