76da1ff2382e099efd32eee04ed5cd5f755a1ac1
perfectyang <2406790623@qq.com>
2020-07-28 14:49:13 +0800

    fix

--- src/app.js
+++ src/app.js
@@ -6,3 +6,39 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
+// function debounce(fn, delay) {
+//   var timer
+//   return function (...args) {
+//     const self = this
+//     if (timer) clearTimeout(timer)
+//     timer = setTimeout(function () {
+//       fn.apply(self, args)
+//     }, delay)
+//   } 
+// }
+
+// function throttle(fn, delay) {
+//   var prvTime = 0
+//   return function (...args) {
+//     const nowTime = new Date().getTime()
+//     if ((nowTime - prvTime) > delay) {
+//       fn.apply(this, args)
+//       prvTime = nowTime
+//     }
+//   }
+// }
+
+// function throttle (fn, delay) {
+//   var flag = true
+//   return function (...args) {
+//     const self = this
+//     if (!flag) return
+//     flag = false
+//     setTimeout(function() {
+//       fn.apply(self, args)
+//       flag = true
+//     }, delay)
+//   }
+// }
